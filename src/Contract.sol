c90343ba9e780e4bcbd9316486de62bf4241fe73
patrionXYZ <patrion@metaboy.run>
2024-04-10 15:00:10 +0900

    Forge settings and first ERC-20 contract

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,4 +1,14 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.13;
 
-contract Contract { }
+import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
+contract YOYOToken is ERC20 {
+    constructor() ERC20("YoyogiCoin", "YOYO") {
+        _mint(msg.sender, 1000000000 * decimals());
+    }
+
+    function decimals() public pure override returns (uint8) {
+        return 0;
+    }
+}
